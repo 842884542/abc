2d9a7b88366f5efbd840a688ef29f45e0b5e4e96
842884542 <34060859+842884542@users.noreply.github.com>
2018-05-23 16:15:06 +0800

    510

--- abc/src/com/abc/Hello.java
+++ abc/src/com/abc/Hello.java
@@ -3,8 +3,6 @@ package com.abc;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("改变三次");
-		for (int i = 0; i < 5; i++) {
-			System.out.println("好没分为i");
-		}
+		System.out.println("不知道能不能提交");
 	}
 }
